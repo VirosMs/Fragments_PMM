dbd5ae54ef37cf4a7ad091c1ff6aa4ff9ba4f0b1
chaarrsan <charlestacodeouro@hotmail.com>
2023-11-14 16:30:07 +0100

    Pratica finalizada

--- app/src/main/java/com/example/fragments/onboarding3Fragment.java
+++ app/src/main/java/com/example/fragments/onboarding3Fragment.java
@@ -15,10 +15,10 @@ import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Onboarding1Fragment} factory method to
+ * Use the {@link onboarding3Fragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Onboarding1Fragment extends Fragment {
+public class onboarding3Fragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,21 +26,36 @@ public class Onboarding1Fragment extends Fragment {
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-
-
     private String mParam1;
     private String mParam2;
 
-    private Button botonSiguiente;
-
-    private Button botonSkip;
-
     private NavController navController;
-    public Onboarding1Fragment() {
+
+    Button botonSiguiente;
+    Button botonSkip;
+
+
+    public onboarding3Fragment() {
         // Required empty public constructor
     }
 
-
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment onboarding3Fragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static onboarding3Fragment newInstance(String param1, String param2) {
+        onboarding3Fragment fragment = new onboarding3Fragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -55,7 +70,7 @@ public class Onboarding1Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_onboarding1, container, false);
+        return inflater.inflate(R.layout.fragment_onboarding3, container, false);
     }
 
     @Override
@@ -67,11 +82,11 @@ public class Onboarding1Fragment extends Fragment {
         botonSiguiente = view.findViewById(R.id.botonSiguiente);
 
         botonSiguiente.setOnClickListener(view1 ->
-                navController.navigate(R.id.action_onboarding1Fragment_to_onboarding2Fragment));
+                navController.navigate(R.id.action_onboarding3Fragment_to_onboarding1Fragment));
 
         botonSkip = view.findViewById(R.id.botonSkip);
 
         botonSkip.setOnClickListener(view1 ->
-                navController.navigate(R.id.action_onboarding1Fragment_to_homeFragment));
+                navController.navigate(R.id.action_onboarding3Fragment_to_homeFragment));
     }
 }
